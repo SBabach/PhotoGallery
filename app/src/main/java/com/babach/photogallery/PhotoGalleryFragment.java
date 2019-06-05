489212cfdd49a2c0c0256b80e00c9ec6404874e6
SBabach <sbabachev-10@unwe.bg>
2019-06-05 21:55:16 +0300

    PhotoGallery Chapter28-Notifications

--- app/src/main/java/com/babach/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/babach/photogallery/PhotoGalleryFragment.java
@@ -1,5 +1,6 @@
 package com.babach.photogallery;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
@@ -96,6 +97,15 @@ public class PhotoGalleryFragment extends Fragment
                 searchView.setQuery(query, false);
             }
         });
+
+        MenuItem toggleItem = menu.findItem(R.id.menu_item_toggle_polling);
+        if (PollService.isServiceAlarmOn(getActivity()))
+        {
+            toggleItem.setTitle(R.string.stop_polling);
+        } else
+        {
+            toggleItem.setTitle(R.string.start_polling);
+        }
     }
 
     @Override
@@ -106,6 +116,11 @@ public class PhotoGalleryFragment extends Fragment
             case R.id.menu_item_clear:
                 QueryPreferences.setStoredQuery(getActivity(), null);
                 updateItems();
+                return true;
+            case R.id.menu_item_toggle_polling:
+                boolean shouldStartAlarm = !PollService.isServiceAlarmOn(getActivity());
+                PollService.setServiceAlarm(getActivity(), shouldStartAlarm);
+                getActivity().invalidateOptionsMenu();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
